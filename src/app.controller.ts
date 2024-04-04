31444dc71eb465096e7a7d963cb0eb3f7d769d27
marcoDmc <marcodamasceno07@gmail.com>
2024-04-04 17:52:47 -0300

    refactor: made changes to the code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,22 @@
 /* eslint-disable prettier/prettier */
-import { Body , Controller , Delete , Get , Param , Post , UploadedFile , UseInterceptors } from "@nestjs/common";
+import {
+  Body ,
+  Controller ,
+  Delete ,
+  Get , Headers ,
+  Post ,
+  UploadedFile , UseInterceptors ,
+} from '@nestjs/common';
 import { AppService } from "./app.service";
-import { FileInterceptor } from "@nestjs/platform-express";
 import { diskStorage } from "multer";
-import e from "express";
-import { DeleteDto } from "../dto/deleteDto";
+import { DeleteDTO } from "../dto/deleteDTO";
+import { CreateUserDTO } from '../dto/createUserDTO';
+import { FileInterceptor } from '@nestjs/platform-express';
+import e from 'express';
+import { GetFileDTO } from '../dto/getFileDTO';
 
 
-@Controller ("upload")
+@Controller ("v1")
 export class AppController {
   constructor ( private readonly appService : AppService ) {
   }
@@ -16,45 +25,47 @@ export class AppController {
   handleMessage(){
     return this.appService.UploadMessage("welcome to my api");
   }
+  @Get ( "user/file/read" )
+  async handleUploadGet (@Body() user : GetFileDTO) : Promise<any> {
+    if(!user.name || !user.email || !user.password) return "operation failed: There's something missing here"
+    return this.appService.GetUploadFileDb (user).then(response => response).catch(error => error);
+  }
 
-  @Post ( "file"
+  @Post ( "user/file/upload"
   )
   @UseInterceptors ( FileInterceptor ( "file" , {
     storage : diskStorage ( {
       destination : "./files" ,
       filename ( _req : e.Request , file : Express.Multer.File , callback : ( error : ( Error | null ) , filename : string ) =>
         void ) {
-        const D = new Date()
-        const year = D.getFullYear();
-        const month = D.getMonth() + 1;
-        const day = D.getDate();
-        const ext = file.originalname.split ( "." );
-        const filename = `${ ext[ 0 ] }-${ day }-${month}-${year}.${ ext[ 1 ] }`;
-
+        const filename = file.originalname;
         callback ( null , filename );
       }
     } )
   } ) )
-  async handleUploadFile ( @UploadedFile () file : Express.Multer.File ) : Promise<any> {
-    if(!file) return  "operation failed: something is missing here";
-    return await this.appService.UploadFileDb ( file ).then(response =>  response).catch(error => error);
+  async handleUploadFile ( @UploadedFile() file: Express.Multer.File, @Headers("email") email: string, @Headers("password") password: string, ) : Promise<any> {
+
+    if(!file ) return  "operation failed: something is missing here";
+    return await this.appService.CreateFileUserDb(file , email , password).then(response =>  response).catch(error => error);
   }
 
-  @Get ( ":name" )
- async handleUploadGet (@Param("name") name: string) : Promise<any> {
-    if(!name) return "operation failed: the name for the file was not provided"
-    return this.appService.GetUploadFileDb (name).then(response => response).catch(error => error);
+  @Post("user/file/create")
+  async handleCreateNewUser(@Body() User: CreateUserDTO) : Promise<any>{
+    if(!User.password || !User.email || !User.name) return "operation failed: something missing here"
+    return await  this.appService.CreateNewUser(User).then(response => response).catch(error => error);
   }
 
-  @Delete ( "delete" )
-async  handleUploadDelete ( @Body () file : DeleteDto ) : Promise<any>{
 
-    if(!file.id) return "operation failed: the identifier for the file was not provided"
-    if(!file.name) return "operation failed: the name for the file was not provided"
+  @Delete ( "user/file/delete" )
+  async  handleUploadDelete ( @Body () file : DeleteDTO , @Headers("email") email: string , @Headers("password") password : string ) : Promise<any>
+
+  {
+
+    if(!file.name || !email || !password) return "operation failed: the name for the file was not provided"
 
     this.appService.DeleteUploadFileLocal ( file).then (  res => res)
 
-    return this.appService.DeleteUploadFileDb ( file ).then ( res => res ).catch ( error =>  error );
+    return this.appService.DeleteUploadFileDb ( file , {email, password }).then ( res => res ).catch ( error =>  error );
 
   }
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { DeleteDto } from '../dto/deleteDto';
+import { DeleteDTO } from '../dto/deleteDTO';
 import * as fs from 'fs-extra';
+import { CreateUserDTO } from '../dto/createUserDTO';
+import { UserDTO } from '../dto/userDTO';
+import { GetFileDTO } from '../dto/getFileDTO';
 
 @Injectable ()
 export class AppService {
@@ -11,91 +14,141 @@ export class AppService {
   UploadMessage(text: string) : string{
     return text
   }
-  async UploadFileDb ( { originalname , size } : Express.Multer.File ) : Promise<any> {
 
-    const date = new Date ().toLocaleDateString ( "pt-BR" );
-    const RandomInteger = Math.floor((Math.random() * 1000)* 1000);
-    const currentTime = new Date ().toLocaleTimeString ( "pt-BR" , { hour12 : false } );
-    const prisma = new PrismaClient ();
-    const ext = originalname.split ( "." );
-    const fileName = `${ ext[ 0 ] }${ RandomInteger }`;
-
-    const fileExists = await prisma.upload.findFirst({
-      where:{
-        File: fileName
-      }
-    });
-
-    if(fileExists) return "operation failed: file name must be unique";
-
-    return prisma.upload.create ( {
-      data : {
-        Date : date ,
-        File : fileName,
-        Size : size ,
-        Time : currentTime
-      }
-    } );
-
-  }
-
-  async GetUploadFileDb (name: string) : Promise<any> {
+  async GetUploadFileDb (user: GetFileDTO) : Promise<any> {
     const prisma = new PrismaClient ();
 
-    const fileExists = await prisma.upload.findFirst({
+    const userExists = await prisma.user.findFirst({
       where:{
+        email: user.email,
+        password: user.password
+      }
+    })
+
+    if(!userExists) return "operation failed: these credentials do not correspond to any user"
+
+    const fileExists = await prisma.file.findFirst({
+      where:{
+        authorId: {
+          equals: userExists.id
+        },
         File: {
-          equals:name,
+          equals: user.name,
         }
       }
     });
     if(!fileExists) return "operation failed: file does not exist"
 
-    return prisma.upload.findMany ( {
-      where : {
-        File : {
-          startsWith : name
-        }
-      }
-    } );
+    return fileExists
   }
 
+  async CreateNewUser(User: CreateUserDTO):Promise<any>{
+    const prisma = new PrismaClient();
+    const rgxEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const rgxName = /^[a-zA-Z\s]+$/;
+    const rgxPwd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const maxLength = 31
 
-  async DeleteUploadFileDb ( file : DeleteDto ) : Promise<any> {
 
-    const prisma = new PrismaClient ();
+    if(!rgxEmail.test(User.email)) return "operation failed: email format not supported"
+    if(!rgxName.test(User.name)) return "operation failed: name format not supported"
+    if(!rgxPwd.test(User.password)) return "operation failed: password format not supported"
+    if(User.name.length > maxLength) return "operation failed: name too long, please abbreviate your name"
 
-    const fileExists = await prisma.upload.count({
+    const userExists = await prisma.user.findFirst({
       where:{
-        id:{
-          equals: file.id
+        name:{
+          equals: User.name
         }
       }
     });
 
-    if(fileExists < 1) return "operation failed: file does not exist"
-    const  checkNumberOfFiles = Number(fileExists) < 1 ? 'file were deleted' : 'files were deleted';
+    if(userExists) return "operation failed: this user already exists"
 
-    await prisma.upload.deleteMany ( {
+    return prisma.user.create ( {
+      data : {
+        name : User.name ,
+        email : User.email ,
+        password : User.password
+      }
+    } );
+  }
+
+  async CreateFileUserDb ( File: Express.Multer.File , email : string , password: string) : Promise<any> {
+
+    const prisma = new PrismaClient();
+
+    const userExists = await prisma.user.findUnique({
+      where:{
+       email: email ,
+        password: password
+      }
+    }).then(response => response)
+
+
+    if(!userExists) return "operation failed: these credentials do not correspond to any user"
+
+    const fileExists = await prisma.file.findFirst({
+      where:{
+        authorId: {
+          equals: userExists.id
+        },File:{
+          equals:File.originalname
+        }
+      }
+    })
+
+    if(fileExists) return "operation failed: file already exists";
+
+    await prisma.user.update ( {
       where : {
-        id : {
-          equals : file.id
+        email : email ,
+        password : password
+      } ,
+      data : {
+        file : {
+          create : {
+            File : File.originalname ,
+            Date : String ( new Date () ) ,
+            Time : String ( new Date () ) ,
+            Size : File.size
+          }
         }
       }
     } );
 
-    return  `operation completed successfully: ${fileExists } ${checkNumberOfFiles}`
+    return "operation completed: add new file";
+
   }
 
-  async DeleteUploadFileLocal ( file : DeleteDto ) : Promise<string>{
+  async DeleteUploadFileDb ( file : DeleteDTO , user: UserDTO ) : Promise<any> {
 
-    const RandomInteger = Math.floor((Math.random() * 1000)* 1000);
+    const prisma = new PrismaClient ();
 
+    const userExists = await prisma.user.findFirst({
+      where:{
+        email: user.email,
+        password: user.password
+      }
+    })
+    if(!userExists) return "operation failed: these credentials do not correspond to any user"
+
+    await prisma.file.deleteMany ( {
+    where:{
+      authorId: {
+        equals: userExists.id
+      },
+      File : file.name
+      }
+    });
+
+    return  `operation completed successfully`
+  }
+
+  async DeleteUploadFileLocal ( file : DeleteDTO ) : Promise<string>{
 
     const folder = "files";
-    const ext = file.name.split(".");
-    const fileName = `${ext[0]}${RandomInteger}`;
-    const filePath = `${folder}/${fileName}`;
+    const filePath = `${folder}/${file.name}`;
 
       try {
         const exist = await fs.pathExists(filePath);
