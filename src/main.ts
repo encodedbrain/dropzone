3f0e91953c50a5d0ad63df7c1b28728f25a22e65
marcoDmc <marcodamasceno07@gmail.com>
2024-04-15 18:19:07 -0300

    fix: trying to solve color problems

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
 }
 bootstrap().then((r) => r);
