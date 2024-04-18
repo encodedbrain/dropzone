2bc89f2a0e78d5620286fa9607ea4094a5e4a1a4
marcoDmc <marcodamasceno07@gmail.com>
2024-04-18 15:24:58 -0300

    chore: setting upload limit size

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 /* eslint-disable prettier/prettier */
 import {
-  Body ,
-  Controller ,
-  Delete ,
-  Get , Headers , Param ,
-  Post , Res ,
-  UploadedFile , UseGuards , UseInterceptors ,
+  Body,
+  Controller,
+  Delete,
+  Get, Headers, Param,
+  Post, Res,
+  UploadedFile, UseGuards, UseInterceptors,
 } from '@nestjs/common';
 import { AppService } from "./app.service";
 import { diskStorage } from "multer";
@@ -16,79 +16,76 @@ import { ReadFileDTO } from '../dto/readFileDTO';
 import { GuardGuard } from './guard/guard.guard';
 import e from 'express';
 
-@Controller ("v1")
+@Controller("v1")
 export class AppController {
-  constructor ( private readonly appService : AppService ) {
+  constructor(private readonly appService: AppService) {
   }
 
   @Get()
-  handleMessage(){
+  handleMessage() {
     return this.appService.UploadMessage("welcome to my api");
   }
 
   @UseGuards(GuardGuard)
-  @Get ( "user/file/read" )
-  async handleGetFileDb (@Body() user : ReadFileDTO) : Promise<any> {
-    if(!user.name || !user.email || !user.password) return "operation failed: There's something missing here"
-    return this.appService.GetFileDb (user).then(response => response).catch(error => error);
+  @Get("user/file/read")
+  async handleGetFileDb(@Body() user: ReadFileDTO): Promise<any> {
+    if (!user.name || !user.email || !user.password) return "operation failed: There's something missing here"
+    return this.appService.GetFileDb(user).then(response => response).catch(error => error);
   }
 
   @UseGuards(GuardGuard)
-  @Get ( "user/file/all" )
-  async handleGetAllFiles(@Body() user : ReadFileDTO) : Promise<any> {
-    if ( !user ) return "operation failed: There's something missing here"
-    return this.appService.GetAllFileDb (user).then(response => response).catch(error => error);
+  @Get("user/file/all")
+  async handleGetAllFiles(@Body() user: ReadFileDTO): Promise<any> {
+    if (!user) return "operation failed: There's something missing here"
+    return this.appService.GetAllFileDb(user).then(response => response).catch(error => error);
   }
 
   @UseGuards(GuardGuard)
   @Get("user/file/download/:filename")
-  async handleGetFileDownload(@Param("filename") filename : string ,  @Headers("email") email : string, @Headers("password") password: string , @Res({ passthrough: true }) res: any) : Promise<any> {
-    if (!email || !password)  return "operation failed: There's something missing here"
-   return await this.appService.DownloadFile({filename,email,password} , res).then(response => response).catch(error => error);
+  async handleGetFileDownload(@Param("filename") filename: string, @Headers("email") email: string, @Headers("password") password: string, @Res({ passthrough: true }) res: any): Promise<any> {
+    if (!email || !password) return "operation failed: There's something missing here"
+    return await this.appService.DownloadFile({ filename, email, password }, res).then(response => response).catch(error => error);
   }
 
   @UseGuards(GuardGuard)
   @Get("user/file/:filename")
-  async handleGetFile(@Param("filename") filename : string , @Res() res : any ) : Promise<any> {
-    return await this.appService.ExposeFile(filename , res).then(response => response).catch(error => error);
+  async handleGetFile(@Param("filename") filename: string, @Res() res: any): Promise<any> {
+    return await this.appService.ExposeFile(filename, res).then(response => response).catch(error => error);
   }
 
-  @Post ( "user/file/upload"
+  @Post("user/file/upload"
   )
   @UseGuards(GuardGuard)
-  @UseInterceptors ( FileInterceptor ( "file" , {
-    storage : diskStorage ( {
-      destination : "./files" ,
-      filename ( _req : e.Request , file : Express.Multer.File , callback : ( error : ( Error | null ) , filename : string ) =>
-        void ) {
+  @UseInterceptors(FileInterceptor("file", {
+    storage: diskStorage({
+      destination: "./files",
+      filename(_req: e.Request, file: Express.Multer.File, callback: (error: (Error | null), filename: string) =>
+        void) {
         const filename = file.originalname;
-        callback ( null , filename );
+        callback(null, filename);
       }
-    } )
-  } ) )
-  async handleCreateFile ( @UploadedFile() file: Express.Multer.File, @Headers("email") email: string, @Headers("password") password: string, ) : Promise<any> {
-
-    if(!file ) return  "operation failed: something is missing here";
-    return await this.appService.CreateFileUserDb(file , email , password).then(response =>  response).catch(error => error);
+    })
+  }))
+  async handleCreateFile(@UploadedFile() file: Express.Multer.File, @Headers("Email") Email: string, @Headers("Password") Password: string,): Promise<any> {
+    if (!file) return "operation failed: something is missing here";
+    return await this.appService.CreateFileUserDb(file, Email, Password).then(response => response).catch(error => error);
   }
 
   @Post("user/create")
-  async handleCreateNewUser(@Body() User: CreateUserDTO) : Promise<any>{
-    if(!User.password || !User.email || !User.name) return "operation failed: something missing here"
-    return await  this.appService.CreateNewUser(User).then(response => response).catch(error => error);
+  async handleCreateNewUser(@Body() User: CreateUserDTO): Promise<any> {
+    if (!User.password || !User.email || !User.name) return "operation failed: something missing here"
+    return await this.appService.CreateNewUser(User).then(response => response).catch(error => error);
   }
 
   @UseGuards(GuardGuard)
-  @Delete ( "user/file/delete" )
-  async  handleDeleteFile ( @Body () file : DeleteDTO , @Headers("email") email: string , @Headers("password") password : string ) : Promise<any>
+  @Delete("user/file/delete")
+  async handleDeleteFile(@Body() file: DeleteDTO, @Headers("email") email: string, @Headers("password") password: string): Promise<any> {
 
-  {
+    if (!file.name || !email || !password) return "operation failed: the name for the file was not provided"
 
-    if(!file.name || !email || !password) return "operation failed: the name for the file was not provided"
+    this.appService.DeleteUploadFileLocal(file).then(res => res)
 
-    this.appService.DeleteUploadFileLocal ( file).then (  res => res)
-
-    return this.appService.DeleteUploadFileDb ( file , {email, password }).then ( res => res ).catch ( error =>  error );
+    return this.appService.DeleteUploadFileDb(file, { email, password }).then(res => res).catch(error => error);
 
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
+import * as express from 'express';
 
 const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+  app.use(express.json({ limit: '100mb' }));
   await app.listen(port);
   app.enableCors();
 }
