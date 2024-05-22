c1f81072fe05fc47c92458bcfb2033728b98c84f
marcoDmc <marcodamasceno07@gmail.com>
2024-05-22 15:18:14 -0300

    test: testing port in entry rule in ec2 instance

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
 import * as express from 'express';
 
-// const port = process.env.PORT || 3000;
-const port = 8000;
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
