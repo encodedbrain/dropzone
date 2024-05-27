7b820cee617c184d92de0e1f84506807fba0be21
marcoDmc <marcodamasceno07@gmail.com>
2024-05-27 11:14:34 -0300

    chore: selected type to be received in the method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,33 @@
+/* eslint-disable prettier/prettier */
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcrypt';
+import { AuthUserDTO } from 'dto/authUserDTO';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
-  ) {}
-  async signIn(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
+  ) { }
+  async signIn(signInDto: AuthUserDTO): Promise<any> {
+
+    const user = await this.usersService.findOne(signInDto);
+    console.log("dados chegou signin", signInDto)
 
     if (!user) throw new UnauthorizedException();
 
-    if (!(await compare(String(pass), String(user?.password)))) {
+    console.log("usuario", user)
+
+    if (!(await compare(String(signInDto.password), String(user?.password)))) {
       throw new UnauthorizedException();
     }
+
     const payload = { sub: user.id, username: user.name };
+
     user.password = '';
+
     return {
       token: await this.jwtService.signAsync(payload),
       user: user,
