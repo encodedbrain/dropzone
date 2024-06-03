54d12d8f00993c92ed3fb9e03548be39a1c18c6e
marcoDmc <marcodamasceno07@gmail.com>
2024-06-03 02:09:27 -0300

    feat: added new controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,43 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { IAuthUserDTO } from '../../types/user/IAuthUserDTO';
 import { ICreateUserDTO } from 'types/user/ICreateUserDTO';
+import { Response } from 'express';
+
 
 
 @Controller('v1')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
   @Post('signin')
-  async signIn(@Body() signInDto: IAuthUserDTO): Promise<any> {
-    if (!signInDto.name || !signInDto.password) return
-    return await this.authService.signIn(signInDto);
+  async signIn(@Body() credential: IAuthUserDTO): Promise<string | any> {
+    if (!credential.name || !credential.password) return "operation failed: something missing here"
+    return await this.authService.signIn(credential);
   }
-
 
   @Post('signup')
-  async handleCreateNewUser(@Body() User: ICreateUserDTO): Promise<any> {
-    if (!User.password || !User.email || !User.name) return "operation failed: something missing here"
-    return await this.authService.signUp(User).then(response => response).catch(error => error);
+  async handleCreateNewUser(@Body() credential: ICreateUserDTO): Promise<string | any> {
+    if (!credential.password || !credential.email || !credential.name) return "operation failed: something missing here"
+    return await this.authService.signUp(credential).then(response => response).catch(error => error);
   }
+
+  @Post('send/email')
+  async handleSendEmail(@Body() email: string, @Res() response: Response): Promise<string> {
+    if (!email) return "operation failed: something missing here"
+    return await this.authService.generateEmail(email, response).then(response => response).catch(error => console.error(error));
+  }
+
+  @Get("forgot-password/:token")
+  async handleForgotPassword(@Param("token") token: string): Promise<boolean> {
+    if (!token) return false
+    return await this.authService.forgotPassword({ token })
+  }
+
+  @Post("change/password")
+  async handleChangePassword(@Body() name: string, @Body() password: string, @Body() newPassword: string, @Res() response: Response): Promise<any> {
+    if (!password || !newPassword) return "operation failed: something missing here"
+    return await this.authService.changePassword({ name, password, newPassword, response });
+  }
+
 }
