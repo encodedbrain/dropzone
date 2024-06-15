5105997cb40991f9423c36e0eb1d5c50a66a87a8
marcodmc <marcodamasceno07@gmail.com>
2024-06-15 06:45:05 -0300

    feat: created new module

---
+++ src/email/email.service.ts
@@ -0,0 +1,22 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+import { ISendMailerDTO } from './../../types/global/global';
+
+@Injectable()
+export class EmailService {
+    constructor(private mailerService: MailerService) { }
+
+    async sendEmail(user: ISendMailerDTO) {
+        const confirmation_url = user.token;
+        console.log(user.email, user.token, user.name)
+        await this.mailerService.sendMail({
+            to: user.email,
+            subject: 'Welcome to DropZone! open and change your password',
+            template: './welcome',
+            context: {
+                name: user.name,
+                confirmation_url,
+            },
+        });
+    }
+}
