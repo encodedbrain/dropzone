579bc288648f3b8afe395b8311330e0893f40572
marcodmc <marcodamasceno07@gmail.com>
2024-06-15 06:46:14 -0300

    chore: configuring the project to understand the ejs type

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import * as process from 'process';
 import * as express from 'express';
+import { join } from 'path';
 
 const port = process.env.PORT || 3000;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
   app.use(express.json({ limit: '100mb' }));
+  app.setViewEngine('ejs');
   await app.listen(port);
   app.enableCors();
 }
