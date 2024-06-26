7264616980c481bd7fdc9d6c7d976391fd96f607
marcodmc <marcodamasceno07@gmail.com>
2024-06-26 13:39:21 -0300

    style: modified controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,7 +14,8 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { GuardGuard } from './guard/guard.guard';
 import e, { Response } from 'express';
 import { General } from '../utils/general';
-import { IReadFileDTO, IDeleteDTO } from "../types/global/global"
+import { IReadFileRouterParameterDTO, IGetAllFileRouterParameterDTO, IDeleteUploadFileDbDTO } from "../types/global/global"
+import { IDeleteUploadFileLocalDTO } from 'types/file/DeleteUploadFileLocalDTO';
 
 
 
@@ -24,33 +25,49 @@ export class AppController {
   }
 
   @Get()
-  handleMessage() {
-    return this.appService.UploadMessage("welcome to my api");
+  handleMessage(@Res() response: Response): Response {
+    const credentials = "welcome to my api"
+    return this.appService.UploadMessage(credentials, response);
   }
 
   @UseGuards(GuardGuard)
   @Get("user/file/read")
-  async handleGetFileDb(@Body() user: IReadFileDTO): Promise<any> {
-    if (!user.name || !user.email || !user.password) return "operation failed: There's something missing here"
-    return this.appService.GetFileDb(user).then(response => response).catch(error => error);
+  async handleGetFileDb(@Body() parameter: IReadFileRouterParameterDTO, @Res() response: Response): Promise<Response> {
+
+    if (!parameter.name || !parameter.email || !parameter.password) return response.status(400).send("operation failed: There's something missing here")
+
+    const credentials = { name: parameter.name, password: parameter.password, email: parameter.email, response }
+
+    return this.appService.GetFileDb(credentials, response).then(response => response).catch(error => error);
   }
 
   @UseGuards(GuardGuard)
   @Get("user/file/all")
-  async handleGetAllFiles(@Body() user: IReadFileDTO): Promise<any> {
-    if (!user) return "operation failed: There's something missing here"
-    return this.appService.GetAllFileDb(user).then(response => response).catch(error => error);
+  async handleGetAllFiles(@Body() parameter: IGetAllFileRouterParameterDTO, @Res() response: Response): Promise<Response> {
+    if (!parameter.email || !parameter.name || !parameter.password) return response.status(400).send("operation failed: There's something missing here")
+
+    const credentials = { name: parameter.name, email: parameter.email, password: parameter.password }
+
+    return this.appService.GetAllFileDb(credentials, response).then(response => response).catch(error => error);
   }
 
-  @Get("user/file/download/:filename")
-  async handleGetFileDownload(@Param("filename") filename: string, @Headers("email") email: string, @Res({ passthrough: true }) response: Response): Promise<StreamableFile | string> {
-    if (!email || !filename) return "operation failed: There's something missing here"
-    return await this.appService.DownloadFile({ filename, email, response }).then(response => response).catch(error => error);
+  @Get("user/file/download/:name")
+  async handleGetFileDownload(@Param("name") name: string, @Headers("email") email: string, @Res({ passthrough: true }) response: Response): Promise<StreamableFile | Response<string>> {
+
+    if (!email || !name) return response.status(400).send("operation failed: There's something missing here")
+
+    const credentials = { name, email }
+
+    return await this.appService.DownloadFile(credentials, response).then(response => response).catch(error => error);
   }
 
   @Get("user/file/:filename")
-  async handleGetFile(@Param("filename") filename: string, @Res() response: any): Promise<any> {
-    return await this.appService.ExposeFile({ filename, response }).then(response => response).catch(error => error);
+  async handleGetFile(@Param("filename") name: string, @Res() response: any): Promise<Response<string> | void> {
+    const credentials = { name }
+
+    if (!name) return response.status(400).send("credentials invalid")
+
+    return await this.appService.ExposeFile(credentials, response).then(response => response).catch(error => error);
   }
 
   @Post("user/file/upload"
@@ -66,21 +83,23 @@ export class AppController {
       }
     })
   }))
-  async handleCreateFile(@UploadedFile() file: Express.Multer.File, @Headers("email") email: string, @Res() response: Response): Promise<string> {
-
-    if (!file) return "operation failed: something is missing here";
-    return await this.appService.CreateFileUserDb({ file, email, response }).then(response => response).catch(error => error);
+  async handleCreateFile(@UploadedFile() file: Express.Multer.File, @Headers("email") email: string, @Res() response: Response): Promise<Response> {
+    const credentials = { file, email }
+    return await this.appService.CreateFileUserDb(credentials, response).then(response => response).catch(error => error)
   }
 
   @UseGuards(GuardGuard)
   @Delete("user/file/delete")
-  async handleDeleteFile(@Body() file: IDeleteDTO, @Headers("email") email: string, @Headers("password") password: string): Promise<any> {
+  async handleDeleteFile(@Body() name: string, @Headers("email") email: string, @Headers("password") password: string, @Res() response: Response): Promise<any> {
 
-    if (!file.name || !email || !password) return "operation failed: the name for the file was not provided"
+    if (!name || !email || !password) return response.status(400).send("operation failed: the name for the file was not provided")
 
-    this.appService.DeleteUploadFileLocal(file).then(response => response)
+    const credentials: IDeleteUploadFileDbDTO = { name, email, password }
+    const deleteFileLocal: IDeleteUploadFileLocalDTO = { name }
 
-    return this.appService.DeleteUploadFileDb(file, { email, password }).then(response => response).catch(error => error);
+    this.appService.DeleteUploadFileLocal(deleteFileLocal).then(response => response)
+
+    return this.appService.DeleteUploadFileDb(credentials, response).then(response => response).catch(error => error);
 
   }
 
