9a8182266feed138d8e0c5a10b37de13fe90cc55
marcodmc <marcodamasceno07@gmail.com>
2024-06-26 13:43:24 -0300

    refactor: refactoring code

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,28 +4,42 @@ import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcrypt';
 import { PrismaClient } from '@prisma/client';
-import { GenerateToken } from 'service/jwt/jwt';
+import { JWT } from 'service/jwt/jwt';
 import { Bcrypt } from 'utils/Bcrypt/Encrypt';
-import { Response } from 'express';
+import { Service } from "utils/service/service"
 import { EmailService } from './../email/email.service';
-import { IAuthUserDTO, ICreateUserDTO, IForgotPasswordDTO, IEncodedDTO, IChangingPasswordDTO } from "../../types/global/global"
+import {
+  IForgotPasswordDTO,
+  IChangingPasswordDTO,
+  IGenerateEmailDTO,
+  ICreateUserDTO,
+  IAuthUserDTO,
+  IDataAuthUserDTO
+} from "../../types/global/global"
+import { Response } from 'express';
 
 const prisma = new PrismaClient()
 
 @Injectable()
-export class AuthService {
+export class AuthService extends JWT {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
-    private emailService: EmailService
-  ) { }
-  async signIn(credential: IAuthUserDTO, @Res() response: Response): Promise<any> {
+    private emailService: EmailService,
+  ) {
+    super(jwtService);
+  }
+  async signIn(credentials: IAuthUserDTO, @Res() response: Response): Promise<IDataAuthUserDTO | Response> {
 
-    const user = await this.usersService.findOne(credential);
+    const { password } = credentials
 
-    if (!user) return response.status(400);
+    if (!Service.handleVerifyPassword(password)) return response.status(400).send("credentials invalid")
 
-    if (!(await compare(String(credential.password), String(user?.password)))) {
+    const user = await this.usersService.findOne(credentials);
+
+    if (!user) return response.status(400).send("invalid user credentials");
+
+    if (!(await compare(password, user.password))) {
       return response.status(401).send("unauthorized")
     }
 
@@ -33,57 +47,51 @@ export class AuthService {
 
     user.password = '';
 
-    const data = {
+    const data: IDataAuthUserDTO = {
       id: user.id,
       name: user.name,
       email: user.email,
-      token: await GenerateToken({ payload, jwt: this.jwtService })
+      token: await this.GenerateToken({ payload, jwt: this.jwtService })
     }
 
     return response.status(200).json(data)
   }
+  async signUp(credentials: ICreateUserDTO, @Res() response: Response): Promise<Response> {
 
-  async signUp(credential: ICreateUserDTO): Promise<any> {
+    Service.handleVerifyCredentialsCreateUser(credentials)
 
-    const rgxEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const rgxName = /^[a-zA-Z\s]+$/;
-    const rgxPwd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    const maxLength = 31
+    const { email, name, password } = credentials
 
-
-    if (!rgxEmail.test(credential.email)) return "operation failed: email format not supported"
-    if (!rgxName.test(credential.name)) return "operation failed: name format not supported"
-    if (!rgxPwd.test(credential.password)) return "operation failed: password format not supported"
-    if (credential.name.length > maxLength) return "operation failed: name too long, please abbreviate your name"
+    if (!Service.handleVerifyEmail(email) || !Service.handleVerifyNickname(name) || !Service.handleVerifyPassword(password))
+      return response.status(400).send("credentials invalid")
 
     const userExists = await prisma.user.findFirst({
       where: {
         name: {
-          equals: credential.name
+          equals: name
         }
       }
-    });
+    })
 
-    if (userExists) return "operation failed: this user already exists"
+    if (userExists) return response.status(400).send("operation failed: this user already exists")
 
-    const encodedPassword = await Bcrypt.EncryptPassword(credential.password);
+    const encodedPassword = await Bcrypt.EncryptPassword(password)
 
     await prisma.user.create({
       data: {
-        name: credential.name,
-        email: credential.email,
+        name: name,
+        email: email,
         password: encodedPassword
       }
-    });
+    })
 
-    return "user created successfully";
+    return response.status(200).send("user created successfully")
   }
+  async generateEmail(credentials: IGenerateEmailDTO, @Res() response: Response): Promise<Response<string>> {
 
-  async generateEmail(credential: string, @Res() response: Response): Promise<any> {
+    const { email } = credentials
 
-    const { email } = Object(credential)
-
-    if (!email) return
+    if (!email || !Service.handleVerifyEmail(email)) return response.status(400).send("credentials invalid")
 
     const user = await prisma.user.findUnique({
       where: {
@@ -91,41 +99,46 @@ export class AuthService {
       }
     })
 
-    if (!user) return "operation failed: this user already exists"
+    if (!user) return response.status(400).send("operation failed: this user already exists")
 
     const payload = { id: user.id, email: user.email }
 
-    const token = await GenerateToken({ payload, jwt: this.jwtService })
+    const token = await this.GenerateToken({ payload, jwt: this.jwtService })
 
     const host = `http://localhost:3000/v1/${token}`
 
     await this.emailService.sendEmail({ name: user.name, email: user.email, token: host });
 
-    return response.status(200).json({ message: "email sent, check your email to change password" })
+    return response.status(200).send("email sent, check your email to change password")
 
   }
+  async forgotPassword(credentials: IForgotPasswordDTO, @Res() response: Response): Promise<Response<string>> {
 
-  async forgotPassword(credential: IForgotPasswordDTO): Promise<any> {
-    const encoded: IEncodedDTO = await this.jwtService.verifyAsync(credential.token)
+    const decoded = await this.VerifyToken(credentials.token).then(res => res).catch(e => e)
 
-    if (!encoded) return credential.res.status(400).json({ token: false })
+    if (!decoded) return response.json({ token: false })
+    else if (!decoded && decoded.response.status == 401) return response.status(401).send(decoded.response.message)
+
+
+    if ((await decoded).id == undefined) return response.status(401).send("not authorized")
 
     const user = await prisma.user.findUnique({
       where: {
-        email: encoded.email
+        id: (await decoded).id,
       }
     })
 
-    if (!user) return false
+    if (!user) return response.status(400).send("credentials invalid")
 
-    return credential.res.status(200).send("redirecting authenticated user")
+    return response.status(200).send("redirecting authenticated user")
 
   }
-  async changePassword(credential: IChangingPasswordDTO): Promise<string | any> {
+  async changePassword(credentials: IChangingPasswordDTO, @Res() response: Response): Promise<Response> {
+    const { name, newPassword, password } = credentials
 
-    const { name } = Object(credential.name)
-    const { password } = Object(credential.password)
-    const { newPassword } = Object(credential.newPassword)
+
+    if (!Service.handleVerifyNickname(name) || !Service.handleVerifyPassword(password) || !Service.handleVerifyPassword(newPassword))
+      return response.status(400).send("credentials invalid")
 
     const user = await prisma.user.findUnique({
       where: {
@@ -133,11 +146,11 @@ export class AuthService {
       }
     })
 
-    if (!user) return credential.response.status(400).send("user not found in our database")
+    if (!user) return response.status(400).send("user not found in our database")
 
     const passwordMatch = await compare(password, user.password);
 
-    if (!passwordMatch) return credential.response.status(400).send("invalid user credentials")
+    if (!passwordMatch) return response.status(400).send("invalid user credentials")
 
     await prisma.user.update({
       where: {
@@ -148,8 +161,6 @@ export class AuthService {
       }
     })
 
-    return credential.response.status(200).send("password updated successfully")
-
-
+    return response.status(200).send("password updated successfully")
   }
 }
