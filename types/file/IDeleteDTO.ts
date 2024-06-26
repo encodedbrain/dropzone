d021148fb57e69aebbd7895fd96bb4ed02d0eb68
marcodmc <marcodamasceno07@gmail.com>
2024-06-26 13:44:03 -0300

    style: deleted files

--- types/file/IDeleteDTO.ts
+++
@@ -1,3 +0,0 @@
-export interface IDeleteDTO {
-  name: string;
-}
--- types/file/IReadFileDownload.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-export interface IReadFileDownloadDTO {
-  filename: string;
-  email: string;
-  response: Response | any
-}
